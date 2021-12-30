117fc212623d8e56757484fc79d6a5983e5733e6
Leonardo Henrique Lopes <leonardoo.lopes126@gmail.com>
2021-12-30 13:35:17 -0300

    Adicionado schedule

--- src/main/java/com/br/segundafase/segundafase/SegundafaseApplication.java
+++ src/main/java/com/br/segundafase/segundafase/SegundafaseApplication.java
@@ -2,11 +2,14 @@ package com.br.segundafase.segundafase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SegundafaseApplication {
 
 	public static void main(String[] args){
 		SpringApplication.run(SegundafaseApplication.class, args);
 	}
+
 }
---
+++ src/main/java/com/br/segundafase/segundafase/schedule/Schedule.java
@@ -0,0 +1,39 @@
+package com.br.segundafase.segundafase.schedule;
+
+import com.br.segundafase.segundafase.service.DemandaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule {
+
+    @Autowired
+    public DemandaService demandaService;
+
+    @Scheduled(fixedDelay = 10000)
+    public void iniciar(){
+
+        boolean resultado = true;
+
+        System.out.println("Iniciando...");
+
+        try {
+
+            System.out.println(demandaService.retornarItemProcurado("Kindle"));
+
+        } catch (Exception e) {
+
+            resultado = false;
+
+            System.out.println("Falha...");
+
+            e.printStackTrace();
+
+        } finally {
+
+            if ( resultado ) System.out.println("Finalizado com sucesso...");
+
+        }
+    }
+}
